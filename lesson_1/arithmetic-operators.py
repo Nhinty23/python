fb4c6c0af94787b2bbe893b5bf2a7c549eb05f19
YenNhi <nhintygithub@gmail.com>
2026-04-06 00:59:32 +0700

    lession_01_arithmetic_operators
    lession_01_arithmetic_operators

---
+++ lesson_1/arithmetic-operators.py
@@ -0,0 +1,24 @@
+print("Hello my name is Nhi")
+
+# Phép cộng
+print(5 + 2)
+
+# Phép trừ
+print(5 - 2)
+
+# Phép nhân
+print(5 * 2)
+
+# Phép chia
+print(5 / 2)
+
+# Phép chia lấy phần nguyên
+print(13 // 5)
+
+# Phép chia lấy phần dư
+print(12 % 9)
+
+# Lũy thừa (mũ)
+print(2 ** 3)
+
+# Nguyễn Thị Yến Nhi - 2026
