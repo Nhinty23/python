fcfe9037a675344d30d270b881980be0e34cb305
YenNhi <nhintygithub@gmail.com>
2026-04-06 22:32:08 +0700

    lesson_1_arithmetic_exams
    lesson_1_arithmetic_exams

---
+++ lesson_1/exam.py
@@ -0,0 +1,74 @@
+print("Hello my name is Nhi")
+
+print("Bài tập liên quan đến toán tử số học")
+
+# 1. Viết chương trình tính tổng của 5 và 7.
+print(f"5 + 7 = {5 + 7}")
+
+# 2. Viết chương trình tính hiệu của 12 trừ đi 4.
+print(f"12 - 4 = {12 - 4}")
+
+# 3. Viết chương trình tính tích của 6 và 8.
+print(f"6 * 8 = {6 * 8}")
+
+# 4. Viết chương trình tính thương của 20 chia cho 5.
+print(f"20 / 5 = {20 / 5}")
+
+# 5. Viết chương trình tính phần nguyên của 17 chia cho 4.
+print(f"17 // 4 = {17 // 4}")
+
+# 6. Viết chương trình tính biểu thức: (5 + 3) × (10 − 4).
+print(f"(5 + 3) * (10 - 4) = {(5 + 3) * (10 - 4)}")
+
+# 7. Viết chương trình tính phần dư của 19 chia cho 5.
+print(f"19 % 5 = {19 % 5}")
+
+# 8. Viết chương trình tính giá trị của 24 (2 lũy thừa 4).
+print(f"2 ** 4 = {2 ** 4}")
+
+# 9. Một ngày có 24 giờ, hãy đổi số giờ đó ra phút.
+print(f"Quy đổi 24 giờ bằng {24 * 60} phút")
+
+# 10. Viết chương trình đổi 20 phút 5 giây sang đơn vị giây.
+print(f"Quy đổi 20 phút 5 giây bằng {20 * 60 + 5} giây")
+
+# 11. Một giờ có 3600 giây. Một ngày có bao nhiêu giây?
+print(f"Quy đổi 3600 giây bằng {3600 * 24} giây")
+
+# 12. Một quyển vở giá 7 nghìn đồng. Mua 5 quyển hết bao nhiêu tiền?
+print(f"Mua 5 quyển vở hết {7 * 5} VNĐ")
+
+# 13. Nam có 10 quả táo. Bình có số táo gấp 3 lần số táo của Nam cộng thêm 2. Hỏi Bình có bao nhiêu quả táo?
+# Nam = 10; Bình = (10 * 3) + 2;
+print(f"Số táo của Bình là {10 * 3 + 2} quả")
+
+# 14. Có 35 chai nước được xếp vào các két, nếu mỗi két chứa được 6 chai, thì có thể xếp được bao nhiêu két và còn dư bao nhiêu chai?
+# Tổng: 35 chai; Két: 6 chai; Số két: 35 // 6 ; Số chai dư: 35 % 6 ;
+print(f"Số két là {35 // 6} két")
+print(f"Số chai dư là {35 % 6} chai")
+
+# 15. Viết chương trình tính diện tích hình chữ nhật có chiều dài 12 và chiều rộng 5.
+print(f"Diện tích của hình chữ nhật là {12 * 5}")
+
+# 16. Cho hình vuông có cạnh bằng 10. Viết chương trình tính chu vi và diện tích hình vuông.
+print(f"Hình vuông có chu vi là {10 * 4} và diện tích là {10 * 10}")
+
+# 17. Viết chương trình tính diện tích hình tròn bán kính r = 7, dùng công thức S = 3.14 × r2.
+print(f"Diện tích hình tròn bán kính r = 7 là {3.14 * (7 ** 2)}")
+
+# 18. Hình vuông thứ nhất có chu vi bằng 24. Hình vuông thứ hai có chu vi bằng 40. Viết chương trình tính xem độ dài một cạnh của hình vuông thứ hai dài hơn độ dài một cạnh của hình vuông thứ nhất bao nhiêu đơn vị?
+# Chu vi 1 = 24
+# Chu vi 2 = 40
+# Cạnh vuông 2 dài hơn vuông 1 bao nhiêu ?
+print(f"Cạnh vuông 2 dài hơn vuông 1 là {(40 // 4) - (24 // 4)}")
+
+# 19. Nếu có 100 viên kẹo chia đều cho 9 bạn, mỗi bạn được bao nhiêu viên (lấy phần nguyên) và còn dư bao nhiêu viên?
+print(f"Mỗi bạn được {100 // 9} viên kẹo và dư {100 % 9} viên kẹo")
+
+# 20. Viết chương trình đổi 2 ngày 5 giờ 30 phút sang đơn vị phút
+# 2 ngày = 24 giờ = (24 * 60) * 2
+# 5 giờ = 5 * 60
+# 30 phút
+print(f"Đổi 2 ngày 5 giờ 30 phút thành {(24 * 60) * 2 + (5 * 60) + 30} phút")
+
+# Nguyễn Thị Yến Nhi - 2026
