6690e04514ff5d81c3c732b853cc76b644145f01
YenNhi <nhintygithub@gmail.com>
2026-04-06 23:28:49 +0700

    lesson_3_input_data
    lesson_3_input_data

---
+++ lesson_3/input_python.py
@@ -0,0 +1,14 @@
+# Syntax: input (prompt)
+nhi = input("Bạn hãy nhập những gì bạn thích nhé !!! \n")
+print("\n")
+
+# Tính tổng
+print("Tính tổng 2 số")
+a = int(input("Nhập số a: "))
+b = int(input("Nhập số b: "))
+sum = a + b
+print(f'Tổng hai số là {sum}')
+
+# Bắt exception khi nhập dữ liệu lỗi ở bài khác nhé
+
+# Nguyễn Thị Yến Nhi - 2026
