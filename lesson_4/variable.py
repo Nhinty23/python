2164fb34057cb0b44010c26d0c891bbc2751cf57
YenNhi <nhintygithub@gmail.com>
2026-04-06 23:55:08 +0700

    lesson_4_variable_in_python
    lesson_4_variable_in_python

---
+++ lesson_4/variable.py
@@ -0,0 +1,22 @@
+# Cú pháp: tên_biến = giá trị gán
+
+# Gán 1 biến cho một giá trị
+nhi = "Xin chào tôi là Nguyễn Thị Yến Nhi <3"
+print(nhi + "\n")
+
+print(f"Kiểu dữ liệu của biến \"nhi\" là {type(nhi)}")
+
+# Gán nhiều biến cho 1 giá trị
+a = b = c = "Bạn hãy variable đi!!!"
+print(a, b, c , "\n")
+
+# Cách đặt tên biến
+# Chỉ đặt tên biến là chữ, số, gạch dưới
+# Phân biêt chữ hoa & chữ thường
+# Không bắt đầu bằng số, không chứa dấu cách trong tên biến
+# Không dùng từ khóa trong python định nghĩa sẵn để đặt tên biến
+
+fruit = "Apple"
+Fruit = "Orange"
+print("I have an ", fruit, "\nI have an ", Fruit)
+
