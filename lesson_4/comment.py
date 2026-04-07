0bd26715b5d29b40a08fbda64f1e9ea7c6c7a8bc
YenNhi <nhintygithub@gmail.com>
2026-04-08 00:06:07 +0700

    lesson_4_comment_update
    lesson_4_comment_update

--- lesson_4/comment.py
+++ lesson_4/comment.py
@@ -1,5 +1,11 @@
 print("Lại là bài về comment nè")
 
+# 1. Comment trong python bắt đầu bằng ký tự #
+# 2. Python sẽ bỏ qua comment khi chạy code nên comment không làm nặng chương trình
+# 3. Comment có thể được viết phía trên hoặc ở cùng dòng với phần code cần được giải thích
+# 4. Khi cần viết comment trên nhiều dòng, đặt dấu # ở đầu mỗi dòng hoặc từ đầu hãy viết comment trong cặp 3 dấu nháy đơn 
+# 5. Chỉ viết comment khi thực sự cần
+
 # Đây là comment một dòng bất kỳ bạn muốn nhấn ctr + / nhe
 
 '''
