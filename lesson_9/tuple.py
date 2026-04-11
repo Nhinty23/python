1a965ab4a3da91871969e2ac93ad72f100b78f31
YenNhi <nhintygithub@gmail.com>
2026-04-11 22:19:40 +0700

    lesson_9_tuple
    lesson_9_tuple

---
+++ lesson_9/tuple.py
@@ -0,0 +1,21 @@
+# 1. Tuple là gì?
+# - Tuple là một cấu trúc dữ liệu trong python dùng để lưu trữ nhiều giá trị trong
+# một biến duy nhất, tương tự như list. Tuy nhiên, khác với list, một tuple sau khi
+# được tạo ra thì sẽ không thể thay đổi được (immutable), tức là không thể thêm,
+# xóa, sửa các giá trị bên trong của tuple
+
+# 2. Các tính chất của tuple
+# - Bất biến / Không thể thay đổi được (immutable)
+# - Có thứ tự: các phần tử được sắp xếp theo thứ tự
+# - Chứa được nhiều kiểu dữ liệu khác nhau
+# - Có thể chứa các dữ liệu trùng nhau
+
+# 3. Một số cách tạo tuple
+# - Tạo tuple bằng dấu ngoặc tròn
+# - Tạo tuple không cần ngoặc tròn
+# - Tạo tuple rỗng
+# - Tạo tuple từ list
+# - Tạo tuple từ chuỗi (str)
+# - Chú ý khi tạo tuple có một phần tử
+
+# Nguyễn Thị Yến Nhi - 2026
