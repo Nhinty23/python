37fc6fca160aaa59466cab12d6f0043e6ad6a7b5
YenNhi <nhintygithub@gmail.com>
2026-04-13 23:40:43 +0700

    lesson_11_dictionary
    lesson_11_dictionary

---
+++ lesson_11/dictionary.py
@@ -0,0 +1,12 @@
+# 1. Dictionary là gì
+# Dictionary cho phép lưu nhiều phần tử, mỗi phần tử có dạng một cặp key-value
+
+# 2. Tính chất:
+# 2.1. Có thể thay đổi (mutable): có thể thêm, xóa, sửa các phần tử
+
+# 2.2. Không có thứ tự cố định ở các bản Python cũ
+# Nhưng từ Python 3.7+ thì giữ thứ tự thêm vào
+
+# 2.3. Key phải duy nhất
+
+# Nguyễn Thị Yến Nhi - 2026
