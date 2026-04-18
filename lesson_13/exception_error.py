94e5f5cc879d287bd7e758a1f1c68695634c60fd
YenNhi <nhintygithub@gmail.com>
2026-04-18 23:34:49 +0700

    lesson_13_exception
    lesson_13_exception_update_exam

--- lesson_13/exception_error.py
+++ lesson_13/exception_error.py
@@ -2,6 +2,8 @@
 # Exception error: Lỗi xảy ra khi chương trình thực hiện
 # một phép tính toán hoặc thao tác không hợp lệ:
 # Ví dụ: chia một số cho số 0, chuyển đổi một chuỗi sang một
+import datetime
+
 
 # Không bắt exception
 # input_number = int(input("Enter a number: "))
@@ -49,6 +51,65 @@ def divide1():
         finally:
             print("The end \n") # Hợp lệ hay không hợp lệ đều chạy vào đây
 
-divide1()
+# divide1()
+
+# Tạo exception với từ khóa "raise"
+# Nhập năm sinh để tính tuổi
+# Có thể viết là datetime.today().year nếu sửa thành from datetime import datetime thay vì là import datetime như hiện tại
+
+def calculate_age():
+    while True:
+        try:
+            year_of_birth = int(input("Enter year of birth: "))
+            current_year = datetime.datetime.today().year # Lấy năm hiện tại
+
+            if year_of_birth <= 0:
+                print("Năm sinh phải > 0\n")
+                continue
+
+            if year_of_birth > current_year:
+                print("Năm sinh không hợp lệ (ở tương lai)\n")
+                continue
+
+            age = current_year - year_of_birth
+
+        except ValueError:
+            print("Vui lòng nhập số nguyên hợp lệ\n")
+            continue
+        else:
+            print(f"Tuổi của bạn là {age} tuổi")
+            break
+
+# calculate_age()
+
+# Tính chu vi , diện tích hình chữ nhật
+def calculate_rectangle():
+    while True:
+        try:
+            length = float(input("Nhập chiều dài: "))
+            width = float(input("Nhập chiều rộng: "))
+
+            if length <= 0 or width <= 0:
+                print("Kích thước phải lớn hơn 0")
+                continue
+
+            if width >= length:
+                print("Chiều rộng phải nhỏ hơn chiều dài")
+                continue
+
+            perimeter = (length + width) * 2
+            area = length * width
+
+        except ValueError:
+            print("Vui lòng nhập số hợp lệ")
+            continue
+        else:
+            print(f"Chu vi: {perimeter:.2f}")
+            print(f"Diện tích: {area:.2f}")
+            break
+        finally:
+            print("Hoàn thành\n")
+
+calculate_rectangle()
 
 # Nguyễn Thị Yến Nhi - 2026
